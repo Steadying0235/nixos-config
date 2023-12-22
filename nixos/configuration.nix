955c10ea86bf0d0efdd778871801221feab7d3b6
steven <github@arrownoon.maskmy.id>
2023-12-22 11:19:53 -0600

    Add laptop power management

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -75,6 +75,7 @@
   boot.resumeDevice = "/dev/disk-by-uuid/e7bac28a-0293-4278-bb00-30ea07c01818";
 #  swapDevices = [ { device = "/dev/disk-by-uuid/e7bac28a-0293-4278-bb00-30ea07c01818"; } ];
 
+  powerManagement.enable = true;
   # from https://www.worldofbs.com/nixos-framework/
   # Suspend-then-hibernate everywhere
   services.logind = {
