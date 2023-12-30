156fbda559ac4e88a34523e4d17639df82c7df4f
steven <github@arrownoon.maskmy.id>
2023-12-30 17:22:05 -0600

    add nvim config

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -55,6 +55,32 @@
   # programs.neovim.enable = true;
   # home.packages = with pkgs; [ steam ];
 
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+    viAlias = true;
+    vimAlias = true;
+    vimdiffAlias = true;
+    withPython3 = true;
+    extraPython3Packages = (ps: with ps; [pynvim]); 
+    plugins = with pkgs.vimPlugins; [
+      nvim-treesitter.withAllGrammars
+      nvim-lspconfig
+      plenary-nvim
+      gruvbox-nvim
+      mini-nvim
+      nvim-cmp
+      telescope-nvim
+      lualine-nvim
+      gitsigns-nvim
+      luasnip
+      comment-nvim
+    ];
+    extraLuaConfig = ''
+      ${builtins.readFile ./nvim/options.lua} 
+    '';
+  };
+
   # Enable home-manager and git
   programs.home-manager.enable = true;
   programs.git = {
