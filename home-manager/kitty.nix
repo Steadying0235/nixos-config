fb03703773165ade25c5c2132305f5fc1e5c967b
steven <github@arrownoon.maskmy.id>
2024-01-17 13:59:45 -0600

    Updated kitty config

--- home-manager/kitty.nix
+++ home-manager/kitty.nix
@@ -3,7 +3,13 @@
   programs.kitty = {
     enable = true;
     font.name = "JetBrainsMono Nerd Font";
-    theme = "Gruvbox Material Dark Hard";
+    theme = "Black Metal";
+    extraConfig = 
+    '' 
+    confirm_os_window_close 0
+    background_opacity 0.7
+    dynamic_background_opacity yes
+    '';
   };
 
 }
